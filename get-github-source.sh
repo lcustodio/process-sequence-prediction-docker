dee091937637901cdae61408406bfdab3633106b
Luis Custodio <luis.custodio@gmail.com>
2017-05-29 17:18:31 +0200

    Fixing the conf :rocket:

--- get-github-source.sh
+++ get-github-source.sh
@@ -1,1 +1,2 @@
-git clone https://github.com/verenich/ProcessSequencePrediction.git
+#git clone https://github.com/verenich/ProcessSequencePrediction.git
+git clone https://github.com/lcustodio/ProcessSequencePrediction.git
